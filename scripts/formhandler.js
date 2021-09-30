84a5874e0981645c4851f729f6c98dcef1e47252
Juan Toledo <oneonet01@outlook.com>
2021-09-29 18:01:53 -0700

    Finished Chp.8, started coffee run website. Can add/delete/retrieve orders

---
+++ scripts/formhandler.js
@@ -0,0 +1,14 @@
+(function (window)
+{
+    'use strict';
+
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    class FormHandler {
+      constructor(selector) {
+	      if (!selector) {throw new Error }
+    App.FormHandler = FormHandler;
+    window.App = App;
+    }
+}(window);
