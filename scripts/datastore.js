08fb805a1b0427bd4dd66ba5fdd85eb64b10f54b
Juan Toledo <oneonet01@outlook.com>
2021-10-19 20:44:09 -0700

    Finished Chp.11, The user is allowed to place an order of their coffee Once the order is submitted, the order will appear under 'Pending Order'

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -3,26 +3,28 @@
 
 	var App = window.App || {};
 
-	function DataStore() {
-		console.log('running the DataStore function');
-		this.data = {};
+	class DataStore {
+		constructor() {
+			console.log('running the DataStore function');
+			this.data = {};
+		}
+		add(key, val) {
+			this.data[key] = val;
+		}
+		get(key) {
+			return this.data[key];
+		}
+		getAll(key) {
+			return this.data;
+		}
+		remove(key) {
+			delete this.data[key];
+		}
 	}
 
-	DataStore.prototype.add = function (key, val) {
-		this.data[key] = val;
-	};
 
-	DataStore.prototype.get = function (key) {
-		return this.data[key];
-	};
 
-	DataStore.prototype.getAll  = function (key) {
-		return this.data;
-	};
 
-	DataStore.prototype.remove  = function (key) {
-		delete this.data[key];
-	};
 
 	App.DataStore = DataStore;
 	window.App = App;
--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -3,43 +3,41 @@
 
   var App = window.App || {};
   var $ = window.jQuery;
-	
-  function FormHandler(selector) {
-   if (!selector) {
-	   throw new Error('No selector provided');
-     }
-   this.$formElement = $(selector);
-   if (this.$formElement.length === 0){
-	   throw new Error('Could not find element with selector: ' +selector);
-	}
+
+  class FormHandler {
+    constructor(selector) {
+      if (!selector) {
+        throw new Error('No selector provided');
+      }
+      this.$formElement = $(selector);
+      if (this.$formElement.length === 0) {
+        throw new Error('Could not find element with selector: ' + selector);
+      }
+    }
+    //Adding the submit handler
+    addSubmitHandler(fn) {
+      console.log('Setting submit handler for form');
+
+      this.$formElement.on('submit', function (event) {
+        event.preventDefault(); //Prevents from leaving the coffeerun page
+
+        //Extracting the data, pg 217
+        var data = {};
+        $(this).serializeArray().forEach(function (item) {
+          data[item.name] = item.value;
+          console.log(item.name + ' is ' + item.value);
+        });
+        console.log(data);
+        fn(data);
+        //UI Enhancements
+        this.reset();
+        this.elements[0].focus();
+      });
+
+    }
   }
-	
-//Adding the submit handler
-  FormHandler.prototype.addSubmitHandler = function() {
-   console.log('Setting submit handler for form');
-
-   this.$formElement.on('submit', function (event) {
-	   event.preventDefault();  //Prevents from leaving the coffeerun page
-//Extracting the data
-	   var data ={};
-	   $(this).serialArray().forEach(function (item) {
-	      data[item.name] = item.value;
-  	      console.log(item.name + ' is ' + item.value);
-	   });
-	     console.log(data);
-	     fn(data);
-//UI Enhancements
-	     this.reset();
-	     this.element[0].focus();
-   });
-
-  };
 
   App.FormHandler = FormHandler;
   window.App = App;
 
 }) (window);
-
-
-
-
